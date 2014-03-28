7d99034aacd9327773fc8a6b7e842c290a8b3bcb
MahmudH <mahmudhssn246@gmail.com>
2014-03-28 09:40:47 +0000

    update gemfile

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,6 +1,9 @@
 class VisitorsController < ApplicationController
-  def new
-  @owner = Owner.new
-    render 'visitors/new' 
-  end
+    def new
+    Rails.logger.debug 'DEBUG: entering new method'
+    @owner = Owner.new
+    Rails.logger.debug 'DEBUG: Owner name is ' + @owner.name 
+    DISASTER  
+    end
 end
+
